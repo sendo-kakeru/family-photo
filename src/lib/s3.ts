4871d3b0c32f905de10a514b93bbbd9b3c180a6f
sendo-kakeru <sendokakeru.js@gmail.com>
2025-10-19 14:05:23 +0900

    fix: remove valibot env

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,13 +1,15 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { env } from "./env";
 
 export const B2_S3_REGION = "us-west-004";
 export const B2_S3_BUCKET = "family-photo";
 
+if (!process.env.B2_KEY_ID || !process.env.B2_APP_KEY) {
+  throw new Error("B2_KEY_ID and B2_APP_KEY must be set");
+}
 export const s3 = new S3Client({
   credentials: {
-    accessKeyId: env.B2_KEY_ID,
-    secretAccessKey: env.B2_APP_KEY,
+    accessKeyId: process.env.B2_KEY_ID,
+    secretAccessKey: process.env.B2_APP_KEY,
   },
   endpoint: `https://s3.${B2_S3_REGION}.backblazeb2.com`,
   forcePathStyle: true,
